0e9cfd6bf4ddc034506b959149b25f657340cf5e
alissonnxavier <alissonalves29@gmail.com>
2025-03-25 11:46:13 -0300

    added functionality to list all videos uploaded on mux

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -5,7 +5,9 @@ import {
     VideoAssetCreatedWebhookEvent,
     VideoAssetErroredWebhookEvent,
     VideoAssetReadyWebhookEvent,
-    VideoAssetTrackCreatedWebhookEvent
+    VideoAssetTrackCreatedWebhookEvent,
+    VideoAssetDeletedWebhookEvent,
+    VideoAssetTrackReadyWebhookEvent
 } from '@mux/mux-node/resources/webhooks';
 import { eq } from 'drizzle-orm';
 import { headers } from 'next/headers';
@@ -16,7 +18,9 @@ type WebhookEvent =
     | VideoAssetCreatedWebhookEvent
     | VideoAssetErroredWebhookEvent
     | VideoAssetReadyWebhookEvent
-    | VideoAssetTrackCreatedWebhookEvent;
+    | VideoAssetTrackCreatedWebhookEvent
+    | VideoAssetTrackReadyWebhookEvent
+    | VideoAssetDeletedWebhookEvent;
 
 export const POST = async (request: Request) => {
     if (!SIGNING_SECRET) {
@@ -56,6 +60,90 @@ export const POST = async (request: Request) => {
                     muxStatus: data.status,
                 })
                 .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+        case "video.asset.ready": {
+            const data = payload.data as VideoAssetReadyWebhookEvent["data"];
+            const playbackId = data.playback_ids?.[0].id;
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            if (!playbackId) {
+                return new Response("Missing playback ID", { status: 400 });
+            }
+
+            const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`;
+            const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`;
+
+            const duration = data.duration ? Math.round(data.duration * 1000) : 0;
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                    muxPlaybackId: playbackId,
+                    muxAssetId: data.id,
+                    thumbnailUrl,
+                    previewUrl,
+                    duration,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.errored": {
+            const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            await db
+                .update(videos)
+                .set({
+                    muxStatus: data.status,
+                })
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.deleted": {
+            const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+
+            if (!data.upload_id) {
+                return new Response("Missing upload ID", { status: 400 });
+            }
+
+            await db
+                .delete(videos)
+                .where(eq(videos.muxUploadId, data.upload_id));
+            break;
+        }
+
+        case "video.asset.track.ready": {
+            const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+                asset_id: string;
+            };
+
+            const assetId = data.asset_id;
+            const trackId = data.id;
+            const status = data.status;
+
+            if (!assetId) {
+                return new Response("Missing asset ID", { status: 400 });
+            }
+
+            console.log("track ready")
+
+            await db
+                .update(videos)
+                .set({
+                    muxTrackId: trackId,
+                    muxTrackStatus: status,
+                })
+                .where(eq(videos.muxAssetId, assetId));
             break;
         }
     }
